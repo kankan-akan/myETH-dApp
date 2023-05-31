e89fb330279cd622cd79e585515b2e49d766ea95
Yusuke Kan <yuh96.excitbb@gmail.com>
2023-05-31 14:24:21 +0900

    initial commit

---
+++ my-wave-portal/scripts/run.js
@@ -0,0 +1,90 @@
+// const main = async () => {
+//   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+//   const waveContract = await waveContractFactory.deploy();
+//   const wavePortal = await waveContract.deployed();
+
+//   console.log("WavePortal address: ", wavePortal.address);
+// };
+
+// const runMain = async () => {
+//   try {
+//     await main();
+//     process.exit(0);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// runMain();
+
+// const main = async () => {
+//   const [owner, randomPerson] = await hre.ethers.getSigners();
+//   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+//   const waveContract = await waveContractFactory.deploy();
+//   const wavePortal = await waveContract.deployed();
+
+//   console.log("Contract deployed to:", wavePortal.address);
+//   console.log("Contract deployed by:", owner.address);
+
+//   let waveCount;
+//   waveCount = await waveContract.getTotalWaves();
+
+//   let waveTxn = await waveContract.wave();
+//   await waveTxn.wait();
+
+//   waveCount = await waveContract.getTotalWaves();
+// };
+
+// const runMain = async () => {
+//   try {
+//     await main();
+//     process.exit(0);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// runMain();
+
+const main = async () => {
+  // const [owner, randomPerson] = await hre.ethers.getSigners();
+  const [owner, randomPerson1, randomPerson2] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  const wavePortal = await waveContract.deployed();
+
+  console.log("Contract deployed to:", wavePortal.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson1).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson2).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
